254f557d6a492ac2828a80c1c1f55c5ae1a2d428
Wisterleys <wister.sousa2@gmail.com>
2021-12-12 17:29:51 -0300

    melhoras iniciais

--- js/main.js
+++ js/main.js
@@ -2,8 +2,17 @@ function clickFile(el){
     return el.parentNode.querySelector('input[type=file]')
 }
 document.addEventListener("DOMContentLoaded",function(){
+    const image = document.querySelector("img")
     document.querySelector('input[type=file]').onchange=function(){
-        console.log(this.files)
+        const read = new FileReader();
+        read.readAsDataURL(this.files[0])
+        read.onload=function () {
+            image.src=read.result
+            console.dir(image)
+            const {offsetHeight,offsetWidth,offsetX} = image
+            console.log(offsetX)
+         }
+        
     }
     const fileBtn = document.querySelectorAll(".btn-get")
     fileBtn.forEach(btn=>{btn.onclick=function(){
