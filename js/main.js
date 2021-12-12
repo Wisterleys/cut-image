af79b54bbe541851182992321b9146f0cc47cc91
Wisterleys <wister.sousa2@gmail.com>
2021-12-12 19:09:10 -0300

    função crop criada

--- js/main.js
+++ js/main.js
@@ -4,9 +4,11 @@
 function clickFile(el){
     return el.parentNode.querySelector('input[type=file]')
 }
+
 document.addEventListener("DOMContentLoaded",function(){
     const selection = document.querySelector(".selection")
-    const image = document.querySelector("img")
+    const photoPreview = document.querySelector("img")
+    const image = new Image();
 
     const EVENTS ={
         mousedown(){
@@ -37,12 +39,13 @@ document.addEventListener("DOMContentLoaded",function(){
             relativeEndX = event.layerX;
             relativeEndY = event.layerY;
             selection.style.visibility="hidden";
+            crop()
         }
     }
 
 
     //Ações
-    Object.keys(EVENTS).forEach(ev=>image.addEventListener(ev,EVENTS[ev]))//Ativação de eventos
+    Object.keys(EVENTS).forEach(ev=>photoPreview.addEventListener(ev,EVENTS[ev]))//Ativação de eventos
     document.querySelector('input[type=file]').onchange=function(){
         const read = new FileReader();
         read.readAsDataURL(this.files[0])
@@ -58,4 +61,62 @@ document.addEventListener("DOMContentLoaded",function(){
     fileBtn.forEach(btn=>{btn.onclick=function(){
         clickFile(this).click()
     } })
+
+    const canvas = document.createElement("canvas");
+    const ctx = canvas.getContext('2d')
+    image.onload=function () {
+        const {height,width}=image
+        canvas.width=width
+        canvas.height=height
+        ctx.clearRect(0,0,width,height)
+        ctx.drawImage(image,0,0)
+        photoPreview.src=canvas.toDataURL()
+      }
+
+      function crop(){
+        const { width: imgW, height: imgH } = image
+        const { width: previewW, height: previewH } = photoPreview
+    
+        const [ widthFactor, heightFactor ] = [ 
+            +(imgW / previewW), 
+            +(imgH / previewH)
+        ]
+    
+        const [ selectionWidth, selectionHeight ] = [
+            +selection.style.width.replace('px', ''),
+            +selection.style.height.replace('px', '')
+        ]
+    
+        const [ croppedWidth, croppedHeight ] = [
+            +(selectionWidth * widthFactor),
+            +(selectionHeight * heightFactor)
+        ]
+    
+        const [actualX, actualY] = [
+            +( relativeStartX * widthFactor ),
+            +( relativeStartY * heightFactor )
+        ]
+    
+        // pegar do ctx a imagem cortada
+        const croppedImage = ctx.getImageData(actualX, actualY, croppedWidth, croppedHeight)
+    
+        // limpar o ctx
+        ctx.clearRect(0,0,ctx.width,ctx.height)
+    
+        // ajuste de proporções
+        image.width = canvas.width = croppedWidth;
+        image.height = canvas.height = croppedHeight;
+    
+        // adicionar a imagem cortada ao ctx
+        ctx.putImageData(croppedImage, 0, 0)
+    
+        // esconder a ferramenta de seleção
+        selection.style.display = 'none'
+    
+        // atualizar o preview da imagem
+        photoPreview.src = canvas.toDataURL()
+    
+        /* // mostrar o botão de download
+        downloadButton.style.display = 'initial' */
+    }
 })
